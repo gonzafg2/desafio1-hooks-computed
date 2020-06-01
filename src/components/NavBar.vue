3244fc39b3089ec11d4840f692158ca6c259b56e
Gonzalo Fleming <gonzafg2@gmail.com>
2020-06-01 14:58:29 -0400

    Desafio OK

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-light py-0">
+    <a class="navbar-brand" href="/">
+      <img
+        src="./../assets/logopoke.png"
+        height="40"
+        class="d-inline-block align-top my-0"
+        alt="Logo"
+      />
+    </a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  background-color: #ef5350;
+}
+</style>
---
+++ src/components/PokeGuia.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container mt-5">
+    <div class="row d-flex justify-content-center">
+      <div class="col-12 my-5 d-flex justify-content-center">
+        <img src="./../assets/logopoke.png" alt="Logo2" />
+      </div>
+      <div class="col-5 d-flex justify-content-center">
+        <div class="input-group my-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text py-0"
+              ><img src="./../assets/logopoke.png" height="30px" alt="Logopoke"
+            /></span>
+          </div>
+          <input
+            v-model="pokeName"
+            type="text"
+            placeholder="Busca un pokemon.."
+            class="form-control"
+            @keyup.enter="getPoke"
+            @keyup.esc="cleanPoke"
+          />
+          <small class="form-text text-muted"
+            >Presiona Enter para buscar y Esc para limpiar y volver a Pikachu.</small
+          >
+        </div>
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-12 d-flex justify-content-center">
+        <h4>{{ getName }}</h4>
+      </div>
+      <div class="col-12 d-flex justify-content-center my-4">
+        <img :src="getImg" alt="Pikachu" />
+      </div>
+      <div class="col-12 d-flex justify-content-center my-3">
+        <h3>Movimientos</h3>
+      </div>
+      <div class="col-12 d-flex justify-content-center">
+        <ul>
+          <li v-for="item in getMoves" :key="item.id">
+            {{ item.move.name }}
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 d-flex justify-content-center my-3">
+        <h3>Habilidades</h3>
+      </div>
+      <div class="col-12 d-flex justify-content-center">
+        <ul>
+          <li v-for="item in pokeAbilities" :key="item.id">
+            {{ item.ability.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokeGuia",
+  data() {
+    return {
+      pokeName: "pikachu",
+      pokeImg: "",
+      pokeMoves: [],
+      pokeAbilities: []
+    };
+  },
+  created() {
+    this.getPoke();
+  },
+  computed: {
+    getName() {
+      return this.pokeName.toUpperCase();
+    },
+    getImg() {
+      return this.pokeImg;
+    },
+    getMoves() {
+      return this.pokeMoves;
+    }
+  },
+  methods: {
+    async getPoke() {
+      await fetch("https://pokeapi.co/api/v2/pokemon/" + this.pokeName)
+        .then(res => res.json())
+        .then(data => {
+          this.pokeImg = data.sprites.front_default;
+          this.pokeMoves = data.moves;
+          this.pokeAbilities = data.abilities;
+        });
+    },
+    cleanPoke() {
+      this.pokeName = "pikachu";
+      this.getPoke();
+    }
+  }
+};
+</script>
