dacbe7c22f0001c1476dc711f0306f541b78c44d
Gonzalo Fleming <gonzafg2@gmail.com>
2021-04-14 00:36:22 -0400

    Update without modern

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express"),
-  serveStatic = require("serve-static"),
-  history = require("connect-history-api-fallback"),
-  port = process.env.PORT || 5000;
+const express = require("express");
+const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
+const port = process.env.PORT || 5000;
 
 const app = express();
 
